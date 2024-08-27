8f341f0620ff78eb46c540776bd1b8611bc5254e
Parisi Daniele <danieleparisi2016@gmail.com>
2024-08-27 16:51:44 +0200

    beautify code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
-import { useCallback, useEffect, useState } from 'react';
-import './App.css';
-import { Table } from './components/table/table';
-import axios from 'axios';
-import { Family } from './utils/types';
-import { IconButton, Snackbar, Typography } from '@mui/material';
-import { Close } from '@mui/icons-material';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { Table } from "./components/table/table";
+import axios from "axios";
+import { Family } from "./utils/types";
+import { IconButton, Snackbar, Typography } from "@mui/material";
+import { Close } from "@mui/icons-material";
 
 function App() {
-
   const [dataset, setDataset] = useState<Family[] | null>(null);
   const [open, setOpen] = useState(false);
   const [snackbarMsg, setSnackbarMsg] = useState("");
-  const [exported,] = useState<Family[]>(JSON.parse(sessionStorage.getItem("exported")!) ?? []);
+  const [exported] = useState<Family[]>(
+    sessionStorage.getItem("exported") === null
+      ? []
+      : JSON.parse(sessionStorage.getItem("exported")!)
+  );
 
   const fetchChildren = useCallback(() => {
     setDataset(null);
-    axios.get('http://localhost:5000/api/children/childrenList').then(t =>
-      setDataset(t.data.children.map((chil: Family) => {
-        return {
-          ...chil,
-          exported: exported.find(ex => {
-            return ex.id === chil.id;
-          }) !== undefined,
-          action: "delete"
-        }
-      }
-      )))
-      .catch(e => { setSnackbarMsg(e.message); setOpen(true); setDataset([]) });
-  }, [exported])
+    axios
+      .get("http://localhost:5000/api/children/childrenList")
+      .then((t) =>
+        setDataset(
+          t.data.children.map((chil: Family) => {
+            return {
+              ...chil,
+              exported:
+                exported.find((ex) => {
+                  return ex.id === chil.id;
+                }) !== undefined,
+              action: "delete",
+            };
+          })
+        )
+      )
+      .catch((e) => {
+        setSnackbarMsg(e.message);
+        setOpen(true);
+        setDataset([]);
+      });
+  }, [exported]);
 
   useEffect(() => {
     fetchChildren();
@@ -35,20 +47,31 @@ function App() {
 
   return (
     <div className="App">
-      <Typography variant='h5' textAlign={"center"} fontWeight={"bold"} fontSize={"24px"}>Family questionairre</Typography>
+      <Typography
+        variant="h5"
+        textAlign={"center"}
+        fontWeight={"bold"}
+        fontSize={"24px"}
+      >
+        Family questionairre
+      </Typography>
       <Table dataset={dataset} load={fetchChildren} exported={exported} />
       <Snackbar
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
         sx={{
           ".MuiPaper-root": {
             backgroundColor: "white",
             color: "black",
-            borderLeft: "5px solid red"
-          }
+            borderLeft: "5px solid red",
+          },
         }}
         open={open}
         message={snackbarMsg}
-        action={<IconButton onClick={() => setOpen(false)}><Close /></IconButton>}
+        action={
+          <IconButton onClick={() => setOpen(false)}>
+            <Close />
+          </IconButton>
+        }
         onClose={() => setOpen(false)}
       />
     </div>
--- src/components/table/deleteSingleMemberCell.tsx
+++ src/components/table/deleteSingleMemberCell.tsx
@@ -6,11 +6,30 @@ import { DeletionConfirmDialog } from "../dialog/deletionConfirm";
 import { DialogProps, Family } from "../../utils/types";
 
 export const DeleteFamilyMemberCell = ({ params, load }: DialogProps) => {
-    const [open, setOpen] = useState(false);
-    return <DeletionConfirmDialog open={open} handleClose={() => setOpen(false)} handleDeleteAll={() => {
-        axios.delete("http://localhost:5000/api/children/deleteById/" + params.id).then(() => {
-            const expt = JSON.parse(sessionStorage.getItem("exported")!) as Family[];
-            sessionStorage.setItem("exported", JSON.stringify(expt.filter((e: Family) => e.id !== params.id)));
-        }).then(() => load());
-    }}><IconButton color="error" size="small" onClick={() => setOpen(true)}><Delete fontSize="small" /></IconButton></DeletionConfirmDialog>
-}
+  const [open, setOpen] = useState(false);
+  return (
+    <DeletionConfirmDialog
+      open={open}
+      handleClose={() => setOpen(false)}
+      handleDeleteAll={() => {
+        axios
+          .delete("http://localhost:5000/api/children/deleteById/" + params.id)
+          .then(() => {
+            const expt = JSON.parse(
+              sessionStorage.getItem("exported")!
+            ) as Family[];
+            if (sessionStorage.getItem("exported"))
+              sessionStorage.setItem(
+                "exported",
+                JSON.stringify(expt?.filter((e: Family) => e.id !== params.id))
+              );
+          })
+          .then(() => load());
+      }}
+    >
+      <IconButton color="error" size="small" onClick={() => setOpen(true)}>
+        <Delete fontSize="small" />
+      </IconButton>
+    </DeletionConfirmDialog>
+  );
+};
